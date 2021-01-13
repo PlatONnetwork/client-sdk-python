b9e2b7e61cd350513a38c59818c0d32ca929c8ff
liyuanchao <liyuanchao@matrixelements.com>
2021-01-13 17:17:19 +0800

    alaya merge to 0.15.1-develop，chainid和hrp解绑，添加获取hrp的api接口，钱包文件删除testnet和mainnet字段

---
+++ tests/get_address_hrp_test.py
@@ -0,0 +1,20 @@
+from client_sdk_python import Web3, HTTPProvider
+from client_sdk_python.eth import PlatON
+
+
+def unit_test_address(url):
+    w3 = Web3(HTTPProvider(url))
+    platon = PlatON(w3)
+    w3.net_type = platon.getAddressHrp
+    w3.init_contract_address(w3)
+    print('net_type is:{}'.format(w3.net_type))
+    print('stakingAddress is:{}'.format(w3.stakingAddress))
+    print('penaltyAddress is:{}'.format(w3.penaltyAddress))
+    print('delegateRewardAddress is:{}'.format(w3.delegateRewardAddress))
+    print('restrictingAddress is:{}'.format(w3.restrictingAddress))
+    print('pipAddress is:{}'.format(w3.pipAddress))
+
+
+if __name__ == '__main__':
+    test_url = "http://127.0.0.1:6789"
+    unit_test_address(test_url)
